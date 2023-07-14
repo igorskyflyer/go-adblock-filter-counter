30c9f5e9dc16b640dd62e9007fca5b3787c79df2
igorskyflyer <igor.dvlpr@gmail.com>
2023-07-14 16:36:30 +0200

    Type variables and check for file existence

--- adblock-filter-counter.go
+++ adblock-filter-counter.go
@@ -8,13 +8,13 @@ import (
 )
 
 func countRules(source string) int {
-	pattern := `\r?\n`
-	regex := regexp.MustCompile(pattern)
-	lines := regex.Split(source, -1)
-	count := 0
+	var pattern string = `\r?\n`
+	var regex *regexp.Regexp = regexp.MustCompile(pattern)
+	var lines []string = regex.Split(source, -1)
+	var count int = 0
 
 	for _, line := range lines {
-		trimmedLine := strings.TrimSpace(line)
+		var trimmedLine string = strings.TrimSpace(line)
 
 		if trimmedLine != "" && !strings.HasPrefix(trimmedLine, "!") {
 			count++
@@ -24,8 +24,29 @@ func countRules(source string) int {
 	return count
 }
 
+func fileExists(filename string) (bool, error) {
+	_, err := os.Stat(filename)
+
+	if err != nil {
+		if os.IsNotExist(err) {
+			return false, nil
+		}
+		return false, err
+	}
+
+	return true, nil
+}
+
 func countFileRules(filename string) int {
+	exists, err := fileExists(filename)
+
+	if !exists {
+		fmt.Println("File not found:", err)
+		return 0
+	}
+
 	source, err := os.ReadFile(filename)
+
 	if err != nil {
 		fmt.Println("Error reading file:", err)
 		return 0
