41b3b30ae0e984dd26dde756e75e9a16b27a8154
igorskyflyer <igor.dvlpr@gmail.com>
2023-07-14 17:06:44 +0200

    Add file counting tests

--- adblock-filter-counter_test.go
+++ adblock-filter-counter_test.go
@@ -12,6 +12,8 @@ const (
 	! Comment
 	||another.test^
 	`
+
+	filePath string = "./data/AdVoid.Core.txt"
 )
 
 func TestCountRules(t *testing.T) {
@@ -35,11 +37,20 @@ func TestCountRules(t *testing.T) {
 }
 
 func TestCountFileRules(t *testing.T) {
-	t.Run("CountFileRules()", func(t *testing.T) {
+	t.Run("CountFileRules() - non-valid file", func(t *testing.T) {
 		count, error := countFileRules("")
 
 		if count > -1 || error != nil {
-			t.Fail()
+			t.Error("Should have errored with a non-valid file, error: ", error)
+		}
+	})
+
+	t.Run("CountFileRules() - valid file", func(t *testing.T) {
+		const expected int = 2495
+		actual, error := countFileRules(filePath)
+
+		if actual != expected || error != nil {
+			t.Errorf("Should have returned %d but returned %d", expected, actual)
 		}
 	})
 }
